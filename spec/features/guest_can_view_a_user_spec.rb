82874a120c4cf1c32e704146812bb5623ac360c6
Mike McKee <mikecm@gmail.com>
2018-10-09 15:25:53 -0600

    removing test comments

--- spec/features/guest_can_view_a_user_spec.rb
+++ spec/features/guest_can_view_a_user_spec.rb
@@ -3,13 +3,9 @@ require 'rails_helper'
 feature 'guest views a user' do
   context 'via /users/:id' do
     scenario 'with a valid user id' do
-      # When I visit "/users/1"
       visit '/users/1'
 
-      # Then I should see the user's name Josiah Bartlet
       expect(page).to have_content('Josiah Bartlet')
-
-      # And I should see the user's email address jbartlet@example.com
       expect(page).to have_content('jbartlet@example.com')
     end
   end
