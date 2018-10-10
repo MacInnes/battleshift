79d23007b8cb42a8b39cfa24f77652440c470586
Mike McKee <mikecm@gmail.com>
2018-10-09 20:22:20 -0600

    adds model test for UserSearchResult

--- spec/models/user_search_result_spec.rb
+++ spec/models/user_search_result_spec.rb
@@ -10,11 +10,23 @@ describe UserSearchResult do
   context 'Instance Methods' do
     context '#user' do
       it 'returns a single specified user' do
-        result = UserSearchResult.new(1)
+        result = UserSearchResult.new(1).user
 
-        expect(result.user).to be_a(User)
-        expect(result.user.name).to eq('Josiah Bartlet')
-        expect(result.user.email).to eq('jbartlet@example.com')
+        expect(result).to be_a(User)
+        expect(result.name).to eq('Josiah Bartlet')
+        expect(result.email).to eq('jbartlet@example.com')
+      end
+    end
+
+    context '#users' do
+      it 'returns a collection of all users' do
+        result = UserSearchResult.new.users
+
+        expect(result).to all(be_a(User))
+        expect(result[0].name).to eq('asdf')
+        expect(result[0].email).to eq('asdf@asdf.com')
+        expect(result[1].name).to eq('Josiah Bartlet')
+        expect(result[1].email).to eq('jbartlet@example.com')
       end
     end
   end
