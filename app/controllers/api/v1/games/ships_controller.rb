71a00463ad8986c02c64780652869431b6fc68c2
Mike McKee <mikecm@gmail.com>
2018-10-16 10:59:25 -0600

    refactors ship controller

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,28 +1,38 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
-    game   = Game.find_by_id(params[:game_id])
-    player = User.find_by_api_key(request.headers['X-API-KEY'])
-    ship   = Ship.new(params['ship_size'])
+    @game   = Game.find_by_id(params[:game_id])
+    @player = User.find_by_api_key(request.headers['X-API-KEY'])
 
-    placer = ShipPlacer.new(
-      board: board_finder(player, game),
-      ship: ship,
-      start_space: params['start_space'],
-      end_space: params['end_space']
-    )
-    placer.run
-    game.save
+    ship_placer.run
+    @game.save
 
-    render json: game, message: placer.message(ship.length)
+    render json: @game, message: ship_placer.message(ship_params[:ship_size])
   end
 
   private
 
+  def ship_params
+    params.permit(:ship_size, :start_space, :end_space, :game_id)
+  end
+
+  def ship_placer_params
+    {
+      board: board_finder(@player, @game),
+      ship: Ship.new(ship_params[:ship_size]),
+      start_space: ship_params[:start_space],
+      end_space: ship_params[:end_space]
+    }
+  end
+
+  def ship_placer
+    @ship_placer ||= ShipPlacer.new(ship_placer_params)
+  end
+
   def board_finder(player, game)
-    if player.api_key == ENV['BATTLESHIFT_API_KEY']
-      game.player_1_board
+    if @player.api_key == ENV['BATTLESHIFT_API_KEY']
+      @game.player_1_board
     else
-      game.player_2_board
+      @game.player_2_board
     end
   end
 end
