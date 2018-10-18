a03056e359b2006a957e6f831fe4e1f18e7d9917
Mike McKee <mikecm@gmail.com>
2018-10-17 20:30:19 -0600

    adds callback methods to game and ship controllers

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,26 +1,44 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      before_action :set_game, only: :show
+      before_action :set_players, :new_game, only: :create
+
       def show
-        game = Game.find_by_id(params[:id])
         render status: 400 unless game
         render json: game if game
       end
 
       def create
-        player1 = User.find_by_api_key(request.headers['X-API-KEY'])
-        player2 = User.find_by_email(params['opponent_email'])
-        game = Game.new(
-          player_1_board: Board.new(4),
-          player_2_board: Board.new(4),
-          player_1_turns: 0,
-          player_2_turns: 0,
-          current_turn: "player_1",
-          player_1: player1,
-          player_2: player2
-        )
         game.save
         render json: game
+      end
+
+      private
+
+      attr_reader :game, :player1, :player2
+
+      def set_game
+        @game ||= Game.find_by_id(params[:id])
+      end
+
+      def set_players
+        @player1 = User.find_by_api_key(request.headers['X-API-KEY'])
+        @player2 = User.find_by_email(params['opponent_email'])
+      end
+
+      def new_game
+        @game ||= Game.new(game_creation_params)
+      end
+
+      def game_creation_params
+        {
+          player_1_board: Board.new(4),
+          player_2_board: Board.new(4),
+          current_turn: 'player_1',
+          player_1: player1,
+          player_2: player2
+        }
       end
     end
   end
